2e894aae8f10e022a31b6d642c6fda0309cb2ca5
ExanlesLuaShit <70239152+ExanlesLuashit@users.noreply.github.com>
2022-10-22 21:43:58 +0200

    Update CustomModules.lua

--- CustomModules.lua
+++ CustomModules.lua
@@ -27,30 +27,20 @@ InfiniteJump = GuiLibrary["ObjectsThatCanBeSaved"]["UtilityWindow"]["Api"].Creat
 
 
 
-    local AntiVoid1 = {["Enabled"] = false}
-	AntiVoid1 = GuiLibrary["ObjectsThatCanBeSaved"]["WorldWindow"]["Api"].CreateOptionsButton({
-		["Name"] = "ApeAntiVoid",
-		["HoverText"] = "Kinda Like Rektskys AntiVoid",
-		["Function"] = function(callback)
-			if callback then
-				local Antivoid2 = Instance.new("Part",game.workspace)
-				Antivoid2.Size = Vector3.new(3000,50,3000)
-				Antivoid2.Anchored = true
-				Antivoid2.CanCollide = true
-				Antivoid2.Position = Vector3.new(0, ylevel, 0)
-				Antivoid2.Name = "Antivoid2"
-			else
-				game:GetService("Workspace").Antivoid2:Destroy() -- This is Not Skided Roxa#6969 Helped Disabling The Module
-			end
-		end
-	})
-
-    ylevel = AntiVoid1.CreateSlider({
-		["Name"] = "YLevel",
-		["Min"] = 1,
-		["Max"] = 12,
-		["HoverText"] = "Epic",
-		["Function"] = function(val)
-			ylevel = val
-		 end
-	})
+MultiAura = GuiLibrary["ObjectsThatCanBeSaved"]["BlatantWindow"]["Api"].CreateOptionsButton({
+        ["Name"] = "MultiAura",
+        ["Function"] = function(callback)
+            if callback then
+                bedwars["PaintRemote"] = getremote(debug.getconstants(KnitClient.Controllers.PaintShotgunController.fire))
+                repeat
+                    task.wait(0.03)
+                    local plrs = GetAllNearestHumanoidToPosition(18.8)
+                    for i,plr in pairs(plrs) do
+                        local selfpos = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
+                        local newpos = plr.Character.HumanoidRootPart.Position
+                        bedwars["ClientHandler"]:Get(bedwars["PaintRemote"]):SendToServer(selfpos, CFrame.lookAt(selfpos, newpos).lookVector)
+                 end
+            end
+        end,
+    })
+end)
