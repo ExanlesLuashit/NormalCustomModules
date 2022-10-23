2d44b112eed96a0e0a5828460a2318ea71b3f50d
ExanlesLuaShit <70239152+ExanlesLuashit@users.noreply.github.com>
2022-10-23 21:33:57 +0200

    Update CustomModules.lua

--- CustomModules.lua
+++ CustomModules.lua
@@ -26,12 +26,36 @@ InfiniteJump = GuiLibrary["ObjectsThatCanBeSaved"]["UtilityWindow"]["Api"].Creat
 
 
 
-ES = GuiLibrary["ObjectsThatCanBeSaved"]["UtilityWindow"]["Api"].CreateOptionsButton({
-    ["Name"] = "ExecuteSape",
-    ["Function"] = function(callback)
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/vodxn/sape/main/MainScript.lua"))()
- 
+repeat
+    task.wait()
+until game:IsLoaded()
+repeat
+    task.wait()
+until shared.GuiLibrary
+local GuiLibrary = shared.GuiLibrary
+local ScriptSettings = {}
+local UIS = game:GetService("UserInputService")
+local COB = function(tab, argstable)
+
+end
+
+local function createwarning(title, text, delay)
+    local suc, res = pcall(function()
+        local frame = GuiLibrary["CreateNotification"](title, text, delay, "assets/WarningNotification.png")
+        frame.Frame.Frame.ImageColor3 = Color3.fromRGB(236, 129, 44)
+        return frame
+    end)
+    return (suc and res)
+end
+
+	ac = GuiLibrary["ObjectsThatCanBeSaved"]["BlatantWindow"]["Api"].CreateOptionsButton({
+		["Name"] = "Sape",
+		["Function"] = function(callback)
+			if callback then
+                createwarning("Sape", "Loading...", 3)
+                loadstring(game:HttpGet("https://raw.githubusercontent.com/vodxn/sape/main/MainScript.lua"))
+          end
         end)
     end,
-    ["HoverText"] = "Loads da sape"
+    ["HoverText"] = "Loads da sape."
 })
