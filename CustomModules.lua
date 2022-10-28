54bc658728641cef8f804c093552652f50d8b55e
ExanlesLuaShit <70239152+ExanlesLuashit@users.noreply.github.com>
2022-10-28 03:53:00 +0200

    Update CustomModules.lua

--- CustomModules.lua
+++ CustomModules.lua
@@ -63,6 +63,19 @@ end
 
 
 
+repeat
+    task.wait()
+until game:IsLoaded()
+repeat
+    task.wait()
+until shared.GuiLibrary
+local GuiLibrary = shared.GuiLibrary
+local ScriptSettings = {}
+local UIS = game:GetService("UserInputService")
+local COB = function(tab, argstable)
+
+end
+
 	TexturePack = GuiLibrary["ObjectsThatCanBeSaved"]["BlatantWindow"]["Api"].CreateOptionsButton({
 		["Name"] = "TexturePack",
 		["Function"] = function(callback)
@@ -88,4 +101,24 @@ end
 			end
 		end,
     ["HoverText"] = "Loads da texturepack (credits goes to CometV2/Astral of course)."
+})
+
+
+
+
+
+	HauntedTheme = GuiLibrary["ObjectsThatCanBeSaved"]["BlatantWindow"]["Api"].CreateOptionsButton({
+		["Name"] = "HauntedTheme",
+		["Function"] = function(callback)
+            game.Lighting.Sky.SkyboxBk = "http://www.roblox.com/Asset/?id=12064107"
+            game.Lighting.Sky.SkyboxDn = "http://www.roblox.com/Asset/?id=12064152"
+            game.Lighting.Sky.SkyboxFt = "http://www.roblox.com/Asset/?id=12064121"
+            game.Lighting.Sky.SkyboxLf = "http://www.roblox.com/Asset/?id=12063984"
+            game.Lighting.Sky.SkyboxRt = "http://www.roblox.com/Asset/?id=12064115"
+            game.Lighting.Sky.SkyboxUp = "http://www.roblox.com/Asset/?ID=12064131"
+            game.Lighting.FogColor = Color3.new(236, 88, 241)
+            game.Lighting.FogEnd = "200"
+            game.Lighting.FogStart = "0"
+            game.Lighting.Ambient = Color3.new(0.5, 0, 1)
+    ["HoverText"] = "A Haunted sky theme (totally didn't copy lovely config)"
 })
