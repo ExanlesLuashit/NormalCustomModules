0d023159b19a6e9c3d399e07f220adc9d57a1f8e
ExanlesLuaShit <70239152+ExanlesLuashit@users.noreply.github.com>
2022-11-02 21:43:34 +0200

    Update CustomModules.lua

--- CustomModules.lua
+++ CustomModules.lua
@@ -63,7 +63,18 @@ end
 
 
 
+repeat
+    task.wait()
+until game:IsLoaded()
+repeat
+    task.wait()
+until shared.GuiLibrary
+local GuiLibrary = shared.GuiLibrary
+local ScriptSettings = {}
+local UIS = game:GetService("UserInputService")
+local COB = function(tab, argstable)
 
+end
 
 	MultiAura = GuiLibrary["ObjectsThatCanBeSaved"]["BlatantWindow"]["Api"].CreateOptionsButton({
 		["Name"] = "MultiAura",
