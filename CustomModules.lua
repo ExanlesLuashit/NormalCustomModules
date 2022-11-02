546a49e4069983772512d03dead0c8c170dcfe6f
ExanlesLuaShit <70239152+ExanlesLuashit@users.noreply.github.com>
2022-11-02 18:12:44 +0200

    Update CustomModules.lua

--- CustomModules.lua
+++ CustomModules.lua
@@ -69,8 +69,34 @@ end
 		["Name"] = "MultiAura",
 		["Function"] = function(callback)
 			if callback then
-   loadstring(game:HttpGet("https://pastebin.com/raw/mLUnn9xw"))()			
-				
+                spawn(function()
+                    repeat
+                        task.wait(0.12)
+                        local nearest = getnearestplayer(Distance["Value"])
+                        if nearest ~= nil and nearest.Team ~= lplr.Team and isalive(nearest) and nearest.Character:FindFirstChild("Humanoid").Health > 0.1 and isalive(lplr) and lplr.Character:FindFirstChild("Humanoid").Health > 0.1 and not nearest.Character:FindFirstChild("ForceField") then
+                            local sword = getSword()
+                            spawn(function()
+                                local anim = Instance.new("Animation")
+                                anim.AnimationId = "rbxassetid://4947108314"
+                                local animator = lplr.Character:FindFirstChild("Humanoid"):FindFirstChild("Animator")
+                                animator:LoadAnimation(anim):Play()
+                                anim:Destroy()
+                                bedwars["ViewmodelController"]:playAnimation(15)
+                            end)
+                            if sword ~= nil then
+                                bedwars["SwordController"].lastAttack = game:GetService("Workspace"):GetServerTimeNow() - 0.11
+                                HitRemote:SendToServer({
+                                    ["weapon"] = sword.tool,
+                                    ["entityInstance"] = nearest.Character,
+                                    ["validate"] = {
+                                        ["raycast"] = {
+                                            ["cameraPosition"] = hashFunc(cam.CFrame.Position),
+                                            ["cursorDirection"] = hashFunc(Ray.new(cam.CFrame.Position, nearest.Character:FindFirstChild("HumanoidRootPart").Position).Unit.Direction)
+                                        },
+                                        ["targetPosition"] = hashFunc(nearest.Character:FindFirstChild("HumanoidRootPart").Position),
+                                        ["selfPosition"] = hashFunc(lplr.Character:FindFirstChild("HumanoidRootPart").Position + ((lplr.Character:FindFirstChild("HumanoidRootPart").Position - nearest.Character:FindFirstChild("HumanoidRootPart").Position).magnitude > 14 and (CFrame.lookAt(lplr.Character:FindFirstChild("HumanoidRootPart").Position, nearest.Character:FindFirstChild("HumanoidRootPart").Position).LookVector * 4) or Vector3.new(0, 0, 0)))
+                                    },
+                                    ["chargedAttack"] = {["chargeRatio"] = 0.8}			
 			end
 		end,
     ["HoverText"] = "A OP MutliAura."
